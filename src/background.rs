91021588552c6e1dcd4b63a25efce2f1e56f8b97
Nik Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-05-28 20:32:35 +0100

    feat: encapsulate `Background` in its own module

---
+++ src/background.rs
@@ -0,0 +1,95 @@
+//! Background of an OG image
+
+use std::{
+    fs, io,
+    path::{Path, PathBuf},
+};
+
+use image::{ImageReader, Rgba, RgbaImage};
+use resvg::{tiny_skia, usvg};
+use tap::Pipe as _;
+use thiserror::Error;
+
+/// Background of the Open Graph image
+#[derive(Clone, Debug)]
+pub enum Background {
+    /// Background is path to an image
+    Image(PathBuf),
+    /// Background is path to an svg
+    Svg(PathBuf),
+    /// The background should be filled with a solid color
+    Fill(Rgba<u8>),
+}
+
+/// Failed to get the background
+#[derive(Debug, Error)]
+pub enum Error {
+    /// Failed to open the image file
+    #[error(transparent)]
+    OpenImage(io::Error),
+    /// Failed to open the svg file
+    #[error(transparent)]
+    OpenSvg(io::Error),
+    /// Failed to decode image
+    #[error(transparent)]
+    DecodeImage(image::ImageError),
+    /// Failed to parse the SVG
+    #[error(transparent)]
+    ParseSvg(usvg::Error),
+    /// Conversion of Svg into Image failed
+    #[error("Failed to convert SVG into an Image")]
+    SvgIntoImage,
+}
+
+impl Background {
+    /// Get the image corresponding to this background
+    pub fn image(
+        &self,
+        width: u32,
+        height: u32,
+    ) -> Result<image::ImageBuffer<Rgba<u8>, Vec<u8>>, Error> {
+        match self {
+            Self::Fill(rgb) => RgbaImage::from_pixel(width, height, *rgb),
+            Self::Image(path) => ImageReader::open(path)
+                .map_err(Error::OpenImage)?
+                .decode()
+                .map_err(Error::DecodeImage)?
+                .into(),
+            Self::Svg(path) => {
+                let mut opt = usvg::Options {
+                    // Get file's absolute directory.
+                    resources_dir: fs::canonicalize(path)
+                        .ok()
+                        .and_then(|p| p.parent().map(Path::to_path_buf)),
+                    ..usvg::Options::default()
+                };
+                opt.fontdb_mut().load_system_fonts();
+
+                let tree = fs::read(path)
+                    .map_err(Error::OpenSvg)?
+                    .pipe(|svg_data| usvg::Tree::from_data(&svg_data, &opt))
+                    .map_err(Error::ParseSvg)?;
+
+                let pixmap_size = tree.size().to_int_size();
+                let mut pixmap =
+                    tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height()).unwrap();
+                resvg::render(&tree, tiny_skia::Transform::default(), &mut pixmap.as_mut());
+
+                RgbaImage::from_raw(
+                    pixmap_size.width(),
+                    pixmap_size.height(),
+                    pixmap
+                        .pixels()
+                        .iter()
+                        .flat_map(|pixel| [pixel.red(), pixel.green(), pixel.blue(), pixel.alpha()])
+                        .collect(),
+                )
+                .ok_or(Error::SvgIntoImage)?
+            }
+        }
+        .pipe(|mut image| {
+            image::imageops::crop(&mut image, 0, 0, width, height);
+            Ok(image)
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,16 @@
-//! Open Graph image generator
+//! OG image generator
 
-use std::{
-    error::Error,
-    fs,
-    path::{Path, PathBuf},
-};
+mod background;
 
-use image::{ImageReader, Rgba, RgbaImage, imageops::crop};
-use resvg::{tiny_skia, usvg};
-use tap::Pipe as _;
+use std::path::Path;
+
+use background::Background;
 
 const WIDTH: u32 = 1280;
 const HEIGHT: u32 = 675;
 
-/// Background of the Open Graph image
-enum Background {
-    /// Background is path to an image
-    Image(PathBuf),
-    /// Background is path to an svg
-    Svg(PathBuf),
-    /// The background should be filled with a solid color
-    Fill(Rgba<u8>),
-}
-
-fn create(bg: Background) -> Result<(), Box<dyn Error>> {
-    let mut img = match bg {
-        Background::Fill(rgb) => RgbaImage::from_pixel(WIDTH, HEIGHT, rgb),
-        Background::Image(path) => ImageReader::open(path)?.decode()?.into(),
-        Background::Svg(path) => {
-            let mut opt = usvg::Options {
-                // Get file's absolute directory.
-                resources_dir: fs::canonicalize(&path)
-                    .ok()
-                    .and_then(|p| p.parent().map(Path::to_path_buf)),
-                ..usvg::Options::default()
-            };
-            opt.fontdb_mut().load_system_fonts();
-
-            let tree = fs::read(&path)?.pipe(|svg_data| usvg::Tree::from_data(&svg_data, &opt))?;
-
-            let pixmap_size = tree.size().to_int_size();
-            let mut pixmap =
-                tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height()).unwrap();
-            resvg::render(&tree, tiny_skia::Transform::default(), &mut pixmap.as_mut());
-
-            RgbaImage::from_raw(
-                pixmap_size.width(),
-                pixmap_size.height(),
-                pixmap
-                    .pixels()
-                    .iter()
-                    .flat_map(|pixel| [pixel.red(), pixel.green(), pixel.blue(), pixel.alpha()])
-                    .collect(),
-            )
-            .ok_or("failed to encode")?
-        }
-    };
-    crop(&mut img, 0, 0, WIDTH, HEIGHT);
+fn create(bg: &Background) -> Result<(), Box<dyn std::error::Error>> {
+    let img = bg.image(WIDTH, HEIGHT)?;
 
     img.save(Path::new("output.png"))?;
 
@@ -64,7 +18,7 @@ fn create(bg: Background) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    create(Background::Svg(
+    create(&Background::Svg(
         Path::new("default-template.svg").to_path_buf(),
     ))
     .unwrap();
