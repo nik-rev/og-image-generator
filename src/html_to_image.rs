7e69475ec0e198aade1fdbedd14475ef84dd8845
Nik Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-05-29 19:14:47 +0100

    feat: support tera templates

--- src/html_to_image.rs
+++ src/html_to_image.rs
@@ -8,21 +8,80 @@ use blitz_traits::{
     ColorScheme, Viewport, navigation::DummyNavigationProvider, net::SharedProvider,
 };
 use image::RgbaImage;
-use std::sync::Arc;
+use std::{
+    fs,
+    path::{self, Path},
+    sync::Arc,
+};
+use tera::{Context, Tera};
 
-pub async fn html_to_image(width: u32, height: u32, html: &str) -> Option<RgbaImage> {
+pub async fn html_to_image(
+    width: u32,
+    height: u32,
+    html_path: &Path,
+    font_path: Option<&Path>,
+    variables: &[(String, String)],
+) -> Option<RgbaImage> {
+    let html = &fs::read_to_string(html_path).unwrap();
     let (mut recv, callback) = MpscCallback::new();
     let callback = Arc::new(callback);
     let net = Arc::new(Provider::new(callback));
 
+    let mut tera_cx = Context::new();
+
+    for (key, value) in variables {
+        tera_cx.insert(key, value);
+    }
+    let html = Tera::default().render_str(html, &tera_cx).unwrap();
+
     let mut html_document = HtmlDocument::from_html(
-        html,
-        None,
+        &format!(
+            "<!doctype html>
+<html>
+    <head>
+        <style>
+            * {{
+              margin: 0px;
+              padding: 0px;
+            }}
+            body {{
+                width: {width}px;
+                height: {height}px;
+                overflow: hidden;
+            }}
+        </style>
+    </head>
+    <body>
+      {html}
+    </body>
+</html>"
+        ),
+        // This allows using relative paths in our HTML file to
+        // import things from the file system
+        Some(format!(
+            "file:///{}",
+            path::absolute(html_path).unwrap().to_str().unwrap()
+        )),
         Vec::new(),
         Arc::clone(&net) as SharedProvider<Resource>,
         None,
         Arc::new(DummyNavigationProvider),
     );
+
+    if let Some(font_path) = font_path {
+        let font = fs::read(font_path).unwrap();
+
+        html_document.font_ctx.collection.register_fonts(
+            font.into(),
+            Some(fontique::FontInfoOverride {
+                family_name: Some(font_path.file_stem().unwrap().to_str().unwrap()),
+                width: None,
+                style: None,
+                weight: None,
+                axes: None,
+            }),
+        );
+    }
 
     html_document
         .as_mut()
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 //! Render OG image to HTML
 
 mod html_to_image;
-use std::{fs, path::PathBuf};
+use std::path::PathBuf;
 
 use clap::Parser as _;
 
@@ -17,6 +17,21 @@ struct Args {
     /// Height of the produced image
     #[arg(long, default_value_t = 675)]
     height: u32,
+    /// Use a custom font. The font's can be referenced in HTML by its filename, excluding the extension
+    /// For example, `Monaspace-Regular.ttf` will be available as `"Monaspace-Regular"`
+    ///
+    /// Only `.ttf` extension is supported.
+    #[arg(long)]
+    font: Option<PathBuf>,
+    /// Variables of format `key=value` that will be available to use in the html.
+    ///
+    /// Each `key` can be used in the HTML as `{{ key }}` and will expand to `value`.
+    #[arg(value_parser = |s: &str|
+        s.split_once('=')
+        .ok_or("expected `key=value`")
+        .map(|(s1, s2)| (s1.to_string(), s2.to_string())
+    ))]
+    variables: Vec<(String, String)>,
 }
 
 #[tokio::main]
@@ -26,7 +41,9 @@ async fn main() {
     let output = html_to_image::html_to_image(
         args.width,
         args.height,
-        &fs::read_to_string(args.input).unwrap(),
+        &args.input,
+        args.font.as_deref(),
+        &args.variables,
     )
     .await
     .unwrap();
